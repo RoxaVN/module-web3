68be5359eab16328a8c50ffe4bc6a06ede6b3eeb
woody <woodpecker146@gmail.com>
2023-08-24 09:25:54 +0700

    feat: init module

---
+++ src/server/migrations/index.ts
@@ -0,0 +1,1 @@
+export * from './1692690369340.init.web3.module.js';
---
+++ src/web/admin/index.ts
@@ -0,0 +1,5 @@
+export * from './providers.js';
+export * from './contracts.js';
+export * from './event.crawlers.js';
+export * from './events.js';
+export * from './event.consumers.js';
