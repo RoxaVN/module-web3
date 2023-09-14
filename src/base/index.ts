00437b39d4b59096217f51f25dd192608ce0f381
woody <woodpecker146@gmail.com>
2023-09-14 16:32:57 +0700

    feat: add utils

--- src/base/index.ts
+++ src/base/index.ts
@@ -1,4 +1,5 @@
 export * from './module.js';
 export * from './access.js';
 export * from './errors.js';
+export * from './utils.js';
 export * from './apis/index.js';
---
+++ src/base/utils.ts
@@ -0,0 +1,3 @@
+export const utils = {
+  isZero: (address: string) => parseInt(address, 16) === 0,
+};
