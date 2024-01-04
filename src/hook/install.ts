40588829a982816b01c2aca8e218343f91322618
woody <woodpecker146@gmail.com>
2024-01-04 15:18:06 +0700

    chore: update with CreateRolesService

--- src/hook/install.ts
+++ src/hook/install.ts
@@ -6,7 +6,7 @@ import { serverModule } from '../server/index.js';
 @serverModule.injectable()
 export class InstallHook extends BaseInstallHook {
   async handle() {
-    await this.createRoleService.handle(roles);
+    await this.createRolesService.handle(roles);
     await this.setAdminRoleService.handle(roles.Admin);
   }
 }
