1801d6b600bccaa8c5a4e21703170bd815c4d297
Toan Nguyen <woodpecker146@gmail.com>
2024-01-06 15:13:11 +0700

    feat: add customChain

--- src/base/utils.ts
+++ src/base/utils.ts
@@ -2,4 +2,14 @@ export const utils = {
   isZero: (address: string) => parseInt(address, 16) === 0,
   shortenAddress: (address: string) =>
     `${address.slice(0, 6)}...${address.slice(-6)}`,
+  customChain: (chainId: string | number, providerUrl: string) => ({
+    id: Number(chainId),
+    name: 'Custom',
+    network: 'custom',
+    nativeCurrency: { decimals: 18, name: 'Custom', symbol: 'CUS' },
+    rpcUrls: {
+      default: { http: [providerUrl] },
+      public: { http: [providerUrl] },
+    },
+  }),
 };
